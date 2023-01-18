1939ec1a48679a4eed53ca2749cdfa983530b8b0
Isra Adel <israsalama4@gmail.com>
2023-01-18 09:39:53 +0400

    Creating HTML CSS JS FILES

---
+++ orders.js
@@ -0,0 +1,64 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping: 'Pending'
+    
+    
+    },
+     {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        paymentStatus:'REFUNDED',
+        shipping: 'Declined'
+    
+    
+    },
+     {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber:'49347',
+        paymentStatus:'Due',
+        shipping: 'Pending'
+    
+    
+    },
+     {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping: 'Pending'
+    
+    
+    },
+
+     {
+        productName: 'Drone With Camera Drone',
+        productNumber:'95623',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    
+    
+    },
+      {
+        productName: 'GPS 4k  Drone',
+        productNumber:'22369',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    
+    
+    },
+    {
+        productName: 'Lozenge  Drone',
+        productNumber:'00455',
+        paymentStatus:'Paid',
+        shipping: 'Delivered'
+    
+    }
+
+
+
+
+
+
+]
